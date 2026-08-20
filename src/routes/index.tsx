838fc02855d6c60c1093d2364df811deea8ea47d
Jorge López <mpssolution2010@gmail.com>
2026-08-20 17:37:49 -0400

    homepage-2026
    Activar deploy Homepage 2026

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+// Homepage 2026 - Property Advisersimport { createFileRoute } from '@tanstack/react-router'
 import SurveyForm from '../components/SurveyForm'
 
 export const Route = createFileRoute('/')({
